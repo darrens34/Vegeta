b4bfc6901890b4c02294ddb837d4012064bee484
Unknown <anaelle.dambreville@gmail.com>
2017-09-22 17:16:56 +0200

    R avec VPD et data nettoyé

--- Anaelle/R/script_R.R
+++ Anaelle/R/script_R.R
@@ -1,24 +1,35 @@
 
 
 # Importation des données:
-path<-"F:/MIASHS/TER/data/EFDC_L2_Flx_FRHes_2010_v010_30m.txt"
-donnees <- read.table(path,sep=",",dec=",",header=T)
+path<-"F:/MIASHS/TER/Vegeta/Anaelle/data/EFDC_L2_Flx_FRHes_2010_v010_30m.txt"
+donnees <- read.table(path,sep=",",dec=",",header=T,na.strings = "-9999")
 head(donnees)
+setwd("F:/MIASHS/TER/Vegeta/Anaelle")
 
 # Supprime les données où -9999 dans sap flow
-donnees <- donnees[-which(donnees$SAP_FLOW =="-9999"),]
+donnees <- donnees[-which(is.na(donnees$SAP_FLOW )),]
+
+# selectionner les heures:
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+donnees$heure <- substrRight(donnees$TIMESTAMP_END,4)
 
 # Transforme tout en numerique
-for (i in 4:33){
+for (i in 4:34){
 donnees[,i]<-as.numeric(as.character(donnees[,i]))}
 
 # Calcul du VPD:
-donnees$RH<-as.numeric(as.character(donnees$RH));donnees$TA<-as.numeric(as.character(donnees$TA))
-VPD <- ((100-donnees$RH)/100) * (610.7 * 10^((7.5*donnees$TA)/(237.3+donnees$TA))) # VPD en Pa
-VPD <- VPD * 10^-3 #VPD en kPa
+es <- 0.6108 * exp((17.27 * donnees$TA) / (donnees$TA + 237.3))
+ea <- donnees$RH/ 100 * es 
+VPD <- es - ea # en kPa
+donnees$VPD <-VPD
+
+# enregistrer le CSV
+write.csv(donnees,"data_ok.csv")
 
 # PDF des correlations
 pdf("scatterplots.pdf")
-for (i in 4:32){
-plot(donnees[,33]~donnees[,i])}
+for (i in c(4:32,34,35)){
+plot(donnees$SAP_FLOW~donnees[,i],ylab="Sap flow",xlab=colnames(donnees)[i])}
 dev.off()
