679fa3e4edb15eda4e9852955127273b549f0add
Unknown <anaelle.dambreville@gmail.com>
2017-09-29 17:29:16 +0200

    equation modele global ok + data des X moyens

--- Anaelle/R/modelisation/modelisation.R
+++ Anaelle/R/modelisation/modelisation.R
@@ -9,11 +9,22 @@ setwd("F:/MIASHS/TER/Vegeta/Anaelle")
 for (i in 4:ncol(donnees)){
   donnees[,i]<-as.numeric(as.character(donnees[,i]))}
 
-donnees_jour<-donnees[donnees$NETRAD_1havant>0,]
-donnees_nuit<-donnees[donnees$NETRAD_1havant<0,]
+# plot des correlations entre les variables
+plot(donnees)
+cor(donnees[,-c(1:3)],use="complete.obs")
 
 # Modele complet:
 mod<-lm(data=donnees,SAP_FLOW~NETRAD_1havant +PPFD_IN_1havant + RH +SW_IN_1havant +TA +FC_1havant +H_1havant+LE+VPD+SB+G)
+summary(mod)
+
+# moyenne des parametres pour chaque heure
+for (i in unique(donnees$heure_solaire)){
+  apply()
+}
+
+donnees_jour<-donnees[donnees$NETRAD_1havant>0,]
+donnees_nuit<-donnees[donnees$NETRAD_1havant<0,]
+
 
 # Modele jour:
 mod_jour<-lm(data=donnees_jour,SAP_FLOW~NETRAD_1havant +PPFD_IN_1havant + RH +SW_IN_1havant +TA +FC_1havant +H_1havant+LE+VPD+SB+G)
